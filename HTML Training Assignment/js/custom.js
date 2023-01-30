e7693dd1d6577695f13300680b97e8cb3d7f6f41
Priyank41241 <priyankshir2001@gmail.com>
2023-01-30 12:35:40 +0530

    Last commit 1

---
+++ HTML Training Assignment/js/custom.js
@@ -0,0 +1,17 @@
+
+
+var fileInput = document.getElementById('upload');
+var noFileSelectedText = 'No file selected';
+fileInput.addEventListener('change', function (e) {
+    var fileName = '';
+    if (this.files && this.files.length > 1) {
+        fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
+    } else {
+        fileName = e.target.value.split('\\').pop();
+    }
+    if (fileName) {
+        document.getElementById('filename').innerHTML = fileName;
+    } else {
+        document.getElementById('filename').innerHTML = noFileSelectedText;
+    }
+});
